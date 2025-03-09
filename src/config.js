b678b03ae96886880ce2427b5aa8b9b68598095d
AmirparsaDD <Amirparsab9@gmail.com>
2025-03-09 19:25:18 +0330

    fixed prompt

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@
 export const API_ENDPOINT = "https://api.avalai.ir/"
 
 export const AI_MODEL = "gemini-2.0-flash-lite"
-export const AI_PROMPT = `Generate an interesting and fun fact in the language defined by the user. The fact should be accurate, engaging, short, complete, and suitable for the specified audience. You may use emojis in your response but do not overuse it. Your response should only contain the fact and nothing else.`
+export const AI_PROMPT = `Generate an interesting and fun fact in the language defined by the user. The fact should be accurate, engaging, short, complete, and suitable for the specified audience. You may use emojis in your response but do not overuse it. Your response should only contain the fact and nothing else, it should not contain a translation, prefix, introduction nor any markdown.`
