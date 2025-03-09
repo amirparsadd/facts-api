deeae239221f46e8031a88e26f52fbd0bd5fb488
AmirparsaDD <Amirparsab9@gmail.com>
2025-03-09 19:21:34 +0330

    improved the prompt

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@
 export const API_ENDPOINT = "https://api.avalai.ir/"
 
 export const AI_MODEL = "gemini-2.0-flash-lite"
-export const AI_PROMPT = `Generate an interesting and fun fact. The fact should be accurate, engaging, short, complete, and suitable for the specified audience. You may use emojis in your response.`
+export const AI_PROMPT = `Generate an interesting and fun fact in the language defined by the user. The fact should be accurate, engaging, short, complete, and suitable for the specified audience. You may use emojis in your response but do not overuse it.`
