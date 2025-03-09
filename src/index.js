b0efa119bfc2d6079d4167fa39732b3c01b4b0fc
AmirparsaDD <Amirparsab9@gmail.com>
2025-03-09 20:09:18 +0330

    fixed issues related to CORS

--- src/index.js
+++ src/index.js
@@ -10,14 +10,30 @@ addEventListener("fetch", (_event) => {
 	event.respondWith(handleRequest(event.request))
 });
 
-async function handleRequest(){
+/**
+ * 
+ * @param {Request} req 
+ * @returns {Response}
+ */
+async function handleRequest(req){
+	const corsHeaders = new Headers()
+	corsHeaders.append("Access-Control-Allow-Headers", "*")
+	corsHeaders.append("Access-Control-Allow-Origin", "*")
+	corsHeaders.append("Access-Control-Allow-Methods", "*")
+
+	if (req.method === "OPTIONS") {
+    return new Response("OK", {
+      headers: corsHeaders
+    });
+  }
+
 	try {
-		return new Response(await getRandomFact())
+		return new Response(await getRandomFact(), { headers: corsHeaders })
 	} catch (error) {
 		log("ERROR", "request-handler-exception", "An error occured while handling a request", {
 			error: JSON.stringify(error)
 		})
 
-		return new Response("An Error Occured!", { status: 500 })
+		return new Response("An Error Occured!", { status: 500 }, { headers: corsHeaders })
 	}
 }
