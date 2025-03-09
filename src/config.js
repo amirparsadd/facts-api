3291f7ce28deae4b9fb304ba9deaa09ba476ac90
AmirparsaDD <Amirparsab9@gmail.com>
2025-03-09 19:14:18 +0330

    moved to a cheaper model

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // The endpoint used for getting completions. Must follow the OPENAI API's format
 export const API_ENDPOINT = "https://api.avalai.ir/"
 
-export const AI_MODEL = "gpt-4o-mini"
+export const AI_MODEL = "gemini-2.0-flash-lite"
 export const AI_PROMPT = `Generate an interesting and fun fact. The fact should be accurate, engaging, short, complete, and suitable for the specified audience. You may use emojis in your response.`
